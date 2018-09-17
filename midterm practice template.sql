cff90f8f4c5dc78d87081e1de938156776189112
linweizeng <41647120+linweizeng@users.noreply.github.com>
2018-09-17 14:07:01 -0700

    Add files via upload

---
+++ midterm practice template.sql
@@ -0,0 +1,18 @@
+/* Put your name and program here
+ * Save the file as first last program.sql and upload into Blackboard */
+
+/* use database spj 
+    relation s describes suppliers
+       sno = supplier number, sname = supplier name, status = status level of supplier, 
+       city = location of supplier, phone = phone number, site = web site url
+    relation p describes parts supplied by suppliers
+       pno = part number, pname = part name, color = color of part, weight = weight in pounds, city = location of part 
+    relations j describes projects
+       jno = project number, jname = project name, city = project location 
+    relation spj describes shipments of parts by suppliers for a particular project 
+    ignore relation sp for now (it is simpler version of spj) 
+    we are going to use just the s table for this assignment */
+
+/* Q1:  Find the names and sites of suppliers with status greater than 20 and city name contains letter i */
+
+/* that's it for practice. Save this sql file now and upload it  */
